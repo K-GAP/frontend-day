3f40c80aec08d277a60ef55149ae7af56d7e5f92
HongDaeEui <day100@enterblock.co>
2023-01-27 18:13:26 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <nav>
+        <NuxtLink to="/login">Go to login</NuxtLink>
+    </nav>
+    <div>index</div>
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <nav>
+            <NuxtLink to = "/">Go to index</NuxtLink>
+        </nav>
+
+        <div>login</div>
+    </div>
+
+</template>
